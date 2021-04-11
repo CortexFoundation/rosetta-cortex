bb731a9aa069c8f21a7b0e34a167a273ee2deb49
ucwong <ucwong@126.com>
2021-04-11 16:02:46 +0800

    rosetta api integrate init

---
+++ doc.go
@@ -0,0 +1,4 @@
+// Package crg holds the source code of Rosetta API bindings for Cosmos SDK.
+// it also provides utils and a service under server/ to expose Rosetta implementation
+// to different interfaces (network layers, command line interfaces, etc.)
+package crg
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/CortexFoundation/rosetta-cortex
+
+go 1.16
+
+require (
+	github.com/coinbase/rosetta-sdk-go v0.5.8
+	google.golang.org/grpc v1.27.0
+)
