e40d14addf4ffddf78a9eba3ab8e93824503a05f
ucwong <ucwong@126.com>
2021-04-12 11:03:00 +0800

    cortex rosetta proxy added

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"github.com/CortexFoundation/rosetta-cortex/proxy"
 	"github.com/CortexFoundation/rosetta-cortex/server"
 )
 
 func main() {
 	settings := server.Settings{}
+	settings.Client = proxy.New()
 	s, _ := server.NewServer(settings)
 	s.Start()
 }
