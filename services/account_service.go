89cadcd6c2806c737f2f4efdf4ce55ce43da0fc1
ucwong <ucwong@126.com>
2021-04-14 14:21:48 +0800

    account coins api

--- services/account_service.go
+++ services/account_service.go
@@ -21,30 +21,30 @@ func NewAccountAPIService(network *types.NetworkIdentifier) server.AccountAPISer
 	}
 }
 
-// Block implements the /block endpoint.
+// Block implements the /account/balance endpoint.
 func (s *AccountAPIService) AccountBalance(
 	ctx context.Context,
 	request *types.AccountBalanceRequest,
 ) (*types.AccountBalanceResponse, *types.Error) {
 	return &types.AccountBalanceResponse{
 		BlockIdentifier: &types.BlockIdentifier{
-			Index: 1000,
-			Hash:  "block 1000",
+			Index: 3616244,
+			Hash:  "0xec87df31c230298a66eabbfa3d030a835831a55ddbefdc958e77e2f7cd59e81d",
 		},
 		Balances: []*Amount{&types.Amount{Value: "1000", Currency: &types.Currency{Symbol: "CTXC", Decimals: 2}}},
 		Metadata: nil,
 	}
 }
 
-// BlockTransaction implements the /block/transaction endpoint.
-func (s *BlockAPIService) BlockTransaction(
+// BlockTransaction implements the /account/coins endpointfunc (s *AccountAPIService) AccountBalance(
+func (s *AccountAPIService) AccountCoins(
 	ctx context.Context,
 	request *types.AccountCoinsRequest,
 ) (*types.AccountCoinsResponse, *types.Error) {
 	return &types.AccountCoinsResponse{
 		BlockIdentifier: &types.BlockIdentifier{
-			Index: 1000,
-			Hash:  "block 1000",
+			Index: 3616244,
+			Hash:  "0xec87df31c230298a66eabbfa3d030a835831a55ddbefdc958e77e2f7cd59e81d",
 		},
 		Coins:    nil,
 		Metadata: nil,
